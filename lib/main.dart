617a178a154f829c119210682f4f48cff6ea3a79
Augusto Veiga <augustoveiga@MacBook-Pro-de-Augusto.local>
2020-05-14 23:29:32 -0300

    sending data from constructor

--- lib/main.dart
+++ lib/main.dart
@@ -14,10 +14,11 @@ class MyApp extends StatelessWidget {
         canvasColor: Color.fromRGBO(255, 254, 229, 1),
         fontFamily: 'Raleway',
         textTheme: ThemeData.light().textTheme.copyWith(
-                headline6: TextStyle(
-              fontSize: 20,
-              fontFamily: 'RobotoCondensed',
-            )),
+              headline6: TextStyle(
+                fontSize: 20,
+                fontFamily: 'RobotoCondensed',
+              ),
+            ),
       ),
       home: CategoriesScreen(),
     );
