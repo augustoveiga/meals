2a34294ed1ba9bd7071712748e8b672726f3ec4a
Augusto Veiga <augustoveiga@MacBook-Pro-de-Augusto.local>
2020-05-14 22:47:52 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'screens/categories_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DeliMeals',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+                headline6: TextStyle(
+              fontSize: 20,
+              fontFamily: 'RobotoCondensed',
+            )),
+      ),
+      home: CategoriesScreen(),
+    );
+  }
+}
