414ad1f485fea590d843a1e1295535572a758b57
Augusto Veiga <augustoveiga@MacBook-Pro-de-Augusto.local>
2020-05-16 00:04:26 -0300

    route for meal_detail

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'screens/categories_screen.dart';
 import 'screens/categories_meals_screen.dart';
+import 'screens/meal_detail_screen.dart';
 import 'utils/app_routes.dart';
 
 void main() => runApp(MyApp());
@@ -26,7 +27,7 @@ class MyApp extends StatelessWidget {
         //home ou tela inicial
         AppRoutes.HOME : (_) => CategoriesScreen(),
         AppRoutes.CATEGORIES_MEALS : (_) => CategoriesMealsScreen(),
-        
+        AppRoutes.MEAL_DETAIL : (_) => MealDeataisScreen(),
       },
     );
   }
