21bf868468223c99ba3ec10fedcd02cb54ae201e
Augusto Veiga <augustoveiga@MacBook-Pro-de-Augusto.local>
2020-05-16 03:15:21 -0300

    developing settings screen with swicht adaptative (filtes)

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,17 +1,77 @@
 import 'package:flutter/material.dart';
 import '../components/main_drawer.dart';
+import '../models/settings.dart';
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends StatefulWidget {
+  @override
+  _SettingsScreenState createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  
+  var settings = Settings();
+  
+  Widget _creatSwitch(
+    String title,
+    String subtitle,
+    bool value,
+    Function onChanged,
+  ) {
+    return SwitchListTile.adaptive(
+      title: Text(title),
+      subtitle: Text(subtitle),
+      value: value,
+      onChanged: onChanged,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Settings'),
-      ),
-      drawer: MainDrawer(),
-      body: Center(
-        child: Text('Settings'),
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Settings'),
+        ),
+        drawer: MainDrawer(),
+        body: Column(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.all(20),
+              child: Text(
+                'Filters',
+                style: Theme.of(context).textTheme.headline6,
+              ),
+            ),
+            Expanded(
+              child: ListView(
+                children: <Widget>[
+                  _creatSwitch(
+                    'Sem Gluten',
+                    'So exibe refeicoes sem gluten',
+                    settings.isGlutenFree,
+                    (value) => setState(() => settings.isGlutenFree = value),
+                  ),
+                  _creatSwitch(
+                    'Sem Lactose',
+                    'So exibe refeicoes sem lactose',
+                    settings.isLactoseFree,
+                    (value) => setState(() => settings.isLactoseFree = value),
+                  ),
+                  _creatSwitch(
+                    'Vegana',
+                    'So exibe refeicoes vegana',
+                    settings.isVegan,
+                    (value) => setState(() => settings.isVegan = value),
+                  ),
+                  _creatSwitch(
+                    'Vegetarianas',
+                    'So exibe refeicoes vegetarianas',
+                    settings.isVegetarian,
+                    (value) => setState(() => settings.isVegetarian = value),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ));
   }
 }
